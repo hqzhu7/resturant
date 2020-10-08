6ed6a95774962893e2811ea828818951b2dcd576
hq zhu <hq.zhu7@gmail.com>
2020-10-08 15:14:21 -0400

    implememnt header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavTabs from './container/NavTabs';
 
 function App() {
   return (
     <div className="App">
+      <NavTabs/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+        <App>
+          <Switch>
+                           
+          </Switch>
+        </App>
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
