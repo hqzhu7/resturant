e86a2e97c69708a90c805acba8a9f3185c952223
hqzhu <hq.zhu@outlook.com>
2020-10-10 19:01:48 -0400

    add style

--- src/container/FoodCategory.jsx
+++ src/container/FoodCategory.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Container from 'react-bootstrap/Container';
+import {ListGroup, Container} from 'react-bootstrap';
 import Food from '../component/Food'
 
 const FoodCategory = ()=>{
@@ -12,10 +11,12 @@ const FoodCategory = ()=>{
             }}>
             <ListGroup variant="flush">
                 <ListGroup.Item as='div'><Food/></ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
+                <ListGroup.Item as='div'><Food/></ListGroup.Item>
             </ListGroup>
         </Container>
     );
